f6ed7f5335016dedbeaa4317781eb9852d8ecce6
abesselem18275 <abesselem.idani@gmail.com>
2018-10-29 13:14:14 +0100

    List enhancement

--- src/app/products/product-navbar/product-navbar.component.ts
+++ src/app/products/product-navbar/product-navbar.component.ts
@@ -17,9 +17,9 @@ export class ProductNavbarComponent implements OnInit {
     iconRegistry.addSvgIcon('familles', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/familles_icon.svg'))
       .addSvgIcon('produits', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/produits_icon.svg'))
       .addSvgIcon('laboratoires', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/laboratoires_icon.svg'));
-    this.navpaths = [new NavPath('Produits', 'product-list', 'produits'),
-              new NavPath('Laboratoires', 'lab-list', 'laboratoires'),
-              new NavPath('Familles', 'reactive-list', 'familles')];
+    this.navpaths = [new NavPath('PRODUITS', 'product-list', 'produits'),
+              new NavPath('LABORATOIRES', 'lab-list', 'laboratoires'),
+              new NavPath('FAMILLES', 'reactive-list', 'familles')];
 
     this.activeLink = this.navpaths[0].path;
   }
