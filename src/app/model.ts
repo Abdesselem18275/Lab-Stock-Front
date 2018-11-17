b0d93beee16773b20c24bb4fafdd3520bee58510
abesselem18275 <abesselem.idani@gmail.com>
2018-11-17 13:31:37 +0100

    pretty

--- src/app/model.ts
+++ src/app/model.ts
@@ -1,17 +1,16 @@
 export class Product {
-    id : number ;
-    famille : any ; 
-    laboratoire : any ; 
-    designation : string ;
-    reference : string ;
-    creation_date : string ;  
-    contenantCoffret : number ; 
-    testContenant : number ; 
-    cmm :number ;  
-    StockMiniMois :number ; 
+    id: number ;
+    famille: any ;
+    laboratoire: any ;
+    designation: string ;
+    reference: string ;
+    creation_date: string ;
+    contenantCoffret: number ;
+    testContenant: number ;
+    cmm: number ;
+    StockMiniMois: number ;
 
-    constructor(obj?: any)
-    {
+    constructor(obj?: any) {
         this.id = obj && obj.id || null ;
         this.famille = obj && obj.famille || null ;
         this.laboratoire = obj && obj.laboratoire || null ;
@@ -66,8 +65,7 @@ export class Famille {
 export class Laboratoire {
     id: number ;
     designation: string ;
-    constructor(obj?: any)
-     {
+    constructor(obj?: any) {
         this.id = obj && obj.id || null;
         this.designation = obj && obj.designation || null;
     }
@@ -81,7 +79,7 @@ export class Laboratoire {
             return null;
         }
     }
-}
+
 
 
 
@@ -89,13 +87,12 @@ export class Laboratoire {
 
 
 export class NavPath {
-    label : string ; 
-    path : string ; 
-    icon_name : string ; 
+    label: string ;
+    path: string ;
+    icon_name: string ;
 
-    constructor(label: string , path : string,icon_name : string)
-    {
-        this.label=label;
+    constructor(label: string , path: string, icon_name: string) {
+        this.label = label;
         this.path = path;
         this.icon_name = icon_name;
     }
